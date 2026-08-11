6b38f759d7d24766baaa708db94fb8275c6316cb
ege <egeknkc@gmail.com>
2026-08-12 00:59:18 +0300

    chore: remove test files and node_modules

--- extract.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('test.html', 'utf8');
-const regex = /<script\b[^>]*src=[\"'](.*?)[\"'][^>]*>/gi;
-let match;
-while ((match = regex.exec(html)) !== null) {
-    console.log(match[1]);
-}
--- extract2.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const text = fs.readFileSync('t.js', 'utf8');
-const strings = text.match(/[\"'](.*?)[\"']/g);
-if (strings) {
-    const unique = [...new Set(strings.map(s => s.slice(1, -1)))];
-    unique.filter(s => s.toLowerCase().includes('twitter') || s.toLowerCase().includes('gtm') || s.toLowerCase().includes('facebook')).forEach(s => console.log(s));
-}
